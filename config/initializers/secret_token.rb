0454b2741bb4bfbb202d9096be0832a8257bf661
Keita Ishibashi <fromjpn@gmail.com>
2013-10-13 02:08:09 +0900

    bundle install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsPracticeOiax::Application.config.secret_key_base = 'c54732c1a8ae9dde368f7b8091d92b5f576bd5986d319d47dfd57c9b711e08f03f97c17995f56651cc2d0eeb8e8568c37626eefd263f7e2398be5592038bca2e'
