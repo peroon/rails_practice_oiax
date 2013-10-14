15701906f214d276142b1f46690d716ba423e990
Keita Ishibashi <fromjpn@gmail.com>
2013-10-15 01:25:48 +0900

    Add text helper

--- app/controllers/lesson_controller.rb
+++ app/controllers/lesson_controller.rb
@@ -1,6 +1,5 @@
 class LessonController < ApplicationController
   def step1
     render text: "こんにちは #{params[:name]} " + request.remote_ip
-
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def textBlue(text)
+    "<p style=\"color:blue\">#{text}</p>".html_safe
+  end
 end
