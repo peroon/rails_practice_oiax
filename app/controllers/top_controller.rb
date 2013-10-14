128b207f9c004b390b89b35d98649b173c21a89c
Keita Ishibashi <fromjpn@gmail.com>
2013-10-15 00:48:41 +0900

    Add about page

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,8 @@
 class TopController < ApplicationController
   def index
+    @message = 'hello rails'
+  end
+
+  def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 RailsPracticeOiax::Application.routes.draw do
   root to: "top#index"
   get "top/index"
+
+  get "about" => "top#about", as: "about"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
