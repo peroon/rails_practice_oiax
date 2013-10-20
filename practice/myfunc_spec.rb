a6560491513ccaf747c80833c6400f9550550793
Keita Ishibashi <fromjpn@gmail.com>
2013-10-21 01:56:35 +0900

    Rewrite RSpec sample

--- practice/myfunc_spec.rb
+++ practice/myfunc_spec.rb
@@ -1,23 +1,37 @@
 require './myfunc.rb'
 
-describe 'aaa' do
-  context 'bbb' do
-    before do
-      a = 1
-      p a
+describe 'about test target' do
+  context 'about context' do
+    before(:all) do
     end
 
-    it 'ccc' do
-      mysum(1,2) == 3
+    before(:each) do
     end
 
-    it 'ddd' do
-      2 == 2
+    it 'about test1' do
+      3.should == mysum(1,2)
     end
 
-    it 'ddd' do
-      2 == 3
+    it 'about test2-1' do
+      [1,2,3].length.should == 3
+    end
+
+    it 'about test2-2' do
+      [1,2,3].should have(3).items
+    end
+
+    #it 'pending test'
+
+    after(:each) do
+    end
+
+    after(:all) do
     end
 
   end
 end
+
+#more readable
+#describe [1,2,3].length do
+#  it { should be_odd }
+#end
