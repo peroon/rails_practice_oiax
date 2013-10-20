21818ede0cbe2c743555f410e1901756bfc302ae
Keita Ishibashi <fromjpn@gmail.com>
2013-10-20 20:00:03 +0900

    Add validation on create & update

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,20 @@
 class Member < ActiveRecord::Base
+
+  validates :number, presence: true, uniqueness: true,
+    numericality: {only_integer: true,
+                   greater_than: 0,
+                   less_than: 100,
+                   allow_blank: true }
+
+  validates :name, presence: true,
+    format: {with: /\A[A-Za-z]\w*\z/, allow_blank: true},
+    length: {minimum: 2, maximum: 20, allow_blank: true},
+    uniqueness: {case_sensitive: false}
+
+  validates :full_name, length: {maximum: 20}
+
+  validates_email_format_of :email
+
   class << self
     def search(query)
       rel = order('number')
