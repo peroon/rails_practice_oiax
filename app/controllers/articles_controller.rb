0dba5501022412f64c7e9fefcdff5b40ff5ed1ec
Keita Ishibashi <fromjpn@gmail.com>
2013-10-31 01:32:52 +0900

    Add article controller

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,46 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.order('released_at DESC')
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article, notice: 'article saved'
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.assign_attributes(article_params)
+    if @article.save
+      redirect_to @article, notice: 'article updated'
+    else
+      render 'edit'
+    end
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :body, :released_at, :expired_at, :member_only)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to :articles
+  end
+end
